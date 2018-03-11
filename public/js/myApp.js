f409faa0e7e5b49936e6ccfbf36b079fd71de006
Luis Constante <luisconstante@yahoo.com>
2018-03-11 00:07:44 +0000

    added a few more pictures

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -21,11 +21,11 @@ app.controller('homeCtrl', ['$scope', '$window', function($scope, $window){
 
 app.controller('galleryCtrl', ['$scope', '$window', function($scope, $window){
 	$scope.images = [];
-	var limit = $window.location.pathname === '/' ? 12 : 63;
+	var limit = $window.location.pathname === '/' ? 12 : 73;
 	console.log(limit);
 	
 	for(var index = 1; index <= limit; index++){
-		if(index !== 10 && index !== 11) {
+		if(index !== 10 && index !== 11 && index !== 2 && index !== 17 && index !== 12 && index !== 20  && index !== 34  && index !== 37  && index !== 39  && index !== 40  && index !== 42 && index !== 45  && index !== 55) {
 			var gallery = {};
 			gallery.image = `/images/con${index}.jpg`;
 			gallery.alt = 'drywall, fences, concrete, earthquake reinforcement, gutters, paint, demolition, carpentry, windows, doors';
